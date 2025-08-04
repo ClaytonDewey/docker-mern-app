36ca57557bb19be92ec69069263723842af89693
Clayton Dewey <clay@dryadmedia.com>
2025-08-04 17:19:08 -0500

    feat: Add session route & controller

---
+++ backend/src/routes/session.route.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { getSessionsHandler } from '../controllers/session.controller';
+
+const sessionRoutes = Router();
+
+// prefix: /sessions
+sessionRoutes.get('/', getSessionsHandler);
+
+export default sessionRoutes;
