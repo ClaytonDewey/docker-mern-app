924b2fdd2c5a280eed73b2255bd508d507435c9c
Clayton Dewey <clay@dryadmedia.com>
2025-08-04 20:54:24 -0500

    create: Add sign in page & function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 export const Home = () => {
   return <div>Home</div>;
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
     </Routes>
   );
 };
